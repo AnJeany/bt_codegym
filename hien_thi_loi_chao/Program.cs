d330cb360243a3566a769a308c7d41c27178d9dc
AnJeany <anthai190205@gmail.com>
2024-01-30 20:49:33 +0700

    hien_thi_loi_chao
    Bai tạp hien thi loi chao W02

---
+++ hien_thi_loi_chao/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hien_thi_loi_chao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+
+            string yourName = Console.ReadLine();
+
+            Console.WriteLine("Hello: " + yourName);
+        }
+    }
+}
