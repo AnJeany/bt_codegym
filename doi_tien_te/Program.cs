ac0487f9f3b8d3a76486f5e468b438de877a77c7
AnJeany <anthai190205@gmail.com>
2024-01-30 21:14:16 +0700

    doi_tien_te
    bai tap doi tieen te w02

---
+++ doi_tien_te/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doi_tien_te
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float usd = float.Parse(Console.ReadLine());
+            float change = usd * 23000;
+            Console.WriteLine(change);
+        }
+    }
+}
