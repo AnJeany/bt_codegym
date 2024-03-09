9b8a31de952a214b1b5d8dedd46012ab584eb088
AnJeany <anthai190205@gmail.com>
2024-03-09 11:32:31 +0700

    LopIndexOutOfRange

---
+++ Tòngile/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tòngile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                FileStream fs = new FileStream("C:\\Users\\ADMIN\\Documents\\TextDemo.txt", FileMode.Open);
+                FileInfo file = new FileInfo(args[0]);
+                if (file.Exists)
+                {
+                    
+                }
+            }
+            catch 
+            { 
+
+            }
+
+        }
+    }
+}
---
+++ Tòngile/ReadTextFileExample.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tòngile
+{
+    internal class ReadTextFileExample
+    {
+        void ReadTextFile(string filePath)
+        {
+            try
+            {
+                FileInfo file = new FileInfo(filePath);
+                if (!file.Exists)
+                {
+                    throw new FileNotFoundException();
+                }
+
+                StreamReader reader = new StreamReader(filePath);
+                string line = "";
+                int sum = 0;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    sum += Int32.Parse(line);
+                }
+                reader.Close();
+                Console.WriteLine("Total: " + sum);
+            }
+            catch (System.Exception)
+            {
+                Console.Error.WriteLine("File not found or invalid content");
+            }
+        }
+    }
+}
